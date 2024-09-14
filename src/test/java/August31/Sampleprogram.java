4272e35880492b1f17846f6eb55177c069ea371e
Harman <kaur.harmn04@gmail.com>
2024-09-13 19:20:35 -0700

    git status

---
+++ src/test/java/August31/Sampleprogram.java
@@ -0,0 +1,12 @@
+package August31;
+
+public class Sampleprogram {
+
+public static void main(String []args){
+String s = "nonohello ";
+String a = "hello";
+System.out.println(s.substring(0,5));
+
+
+}
+}
